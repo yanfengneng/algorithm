8cfbe5c28cf6760e944c6c0e8710384c5bc6b9be
xiaonengfast <2412553598@qq.com>
2022-05-03 19:51:01 +0800

    update

--- test/1.cpp
+++ test/1.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 using namespace std;
 
-struct s1{
-    int a;//0-3
-    char b;//4
-    int c;//8-11
-    double d;//16-23
-};
-
 int main()
 {
-    cout<<sizeof(s1)<<endl;
+    cout<<"hello world!"<<endl;
     return 0;
 }
