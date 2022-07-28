7e98bb7d971d9557b2cda72992ad5336b0f4d77b
xiaonengfast <2412553598@qq.com>
2022-07-28 12:01:54 +0800

    扩展欧几里得算法

---
+++ acwing/acwing_basic/004/877_exgcd.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int exgcd(int a,int b,int &x,int &y)
+{   
+    // a与0的最大公约数为a，此时的公式为 a*x+0*y=a，那么满足条件的 x=1 y=0即可
+    if(b==0){
+        x=1,y=0;
+        return a;
+    }
+    // 翻转 x 和 y 的位置，更加有利于后面的计算
+    int d=exgcd(b,a%b,y,x);
+    // 计算公式：y*b+x*(a%b)=d，其中 a%b=a-c*b （c为a/b的商）
+    // 代入公式：y*b + x*(a-c*b) = d ==>> a*x + b*(y-c*x) =d，所以此时的 y=y-c*x
+    y=y-a/b*x;
+    return d;
+}
+
+int  main()
+{
+
+    int n;scanf("%d",&n);
+    while(n--)
+    {
+        int a,b;scanf("%d%d",&a,&b);
+        int x,y;
+        exgcd(a,b,x,y);
+        printf("%d %d\n",x,y);
+    }
+    return 0;
+}
