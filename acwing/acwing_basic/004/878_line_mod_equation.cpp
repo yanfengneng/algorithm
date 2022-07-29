186ee91fc4dd753b8ee1db8fce08ad80f5e8f294
xiaonengfast <2412553598@qq.com>
2022-07-29 10:06:04 +0800

    增加注解

--- acwing/acwing_basic/004/878_line_mod_equation.cpp
+++ acwing/acwing_basic/004/878_line_mod_equation.cpp
@@ -24,6 +24,9 @@ int  main()
     {
         int a,b,m;scanf("%d%d%d",&a,&b,&m);
         int x,y;
+        // 题目要求的方程为：a*x=m*y+b 其中y为商。移项得：a*x+m*y1=b 其中y1=-y，由于题目只要求我们求x，所以不用管y。
+        // 上式就转换为扩展欧几里得等式了，我们先求出来a*x+m*y1 = gcd(a,m) = d的最大公约数d，然后判断b是否是d的倍数即可。若b是d的倍数，则说明有解；否则就无解。
+        // 若有解，只需要将等式两边同时扩大 b/d 倍就可以得到等式右边的 b 了，即(b/d)*(a*x+m*y1)=(b/d)*d ==>> a*x*b/d+m*y*b/d = b 所以此时a的系数 x*b/d 即为题目所求的解。
         int d=exgcd(a,m,x,y);
         if(b%d)puts("impossible");
         else printf("%d\n",1LL*b/d*x%m);
