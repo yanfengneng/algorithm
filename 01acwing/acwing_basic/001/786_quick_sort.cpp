ba25069f621754feebe6e1e235570da5078768af
yanfengneng <2412553598@qq.com>
2024-09-21 20:33:57 +0800

    Update Today.

--- 01acwing/acwing_basic/001/786_quick_sort.cpp
+++ 01acwing/acwing_basic/001/786_quick_sort.cpp
@@ -4,30 +4,29 @@ using namespace std;
 const int N = 1e5 + 10;
 int q[N], n, k;
 
-int quick_sort(int l,int r,int k)
-{
-	// 区间内只有一个数了，该数为第k小的数
-	if(l==r)return q[l];
+int quick_sort(int l, int r, int k) {
+  // 区间内只有一个数了，该数为第k小的数
+  if (l == r) return q[l];
 
-	int x=q[l+r>>1],i=l-1,j=r+1;
-	while(i<j)
-	{
-		do i++;while(q[i]<x);
-		do j--;while(q[j]>x);
-		if(i<j)swap(q[i],q[j]);
-	}
-	// 小于等于x的个数有cnt
-	int cnt=j-l+1;
-	// 若k<=cnt，那么我们在左边寻找第k小的数
-	if(k<=cnt)return quick_sort(l,j,k);
-	// 若k>cnt，那么我们在右边寻找第k-cnt小的数
-	return quick_sort(j+1,r,k-cnt);
+  int x = q[l + r >> 1], i = l - 1, j = r + 1;
+  while (i < j) {
+    do i++;
+    while (q[i] < x);
+    do j--;
+    while (q[j] > x);
+    if (i < j) swap(q[i], q[j]);
+  }
+  // 小于等于x的个数有cnt
+  int cnt = j - l + 1;
+  // 若k<=cnt，那么我们在左边寻找第k小的数
+  if (k <= cnt) return quick_sort(l, j, k);
+  // 若k>cnt，那么我们在右边寻找第k-cnt小的数
+  return quick_sort(j + 1, r, k - cnt);
 }
 
-int main()
-{
-    scanf("%d %d",&n,&k);
-	for (int i = 0; i < n; ++i)scanf("%d",&q[i]);
-    cout<<quick_sort(0,n-1,k)<<endl;
-	return 0;
+int main() {
+  scanf("%d %d", &n, &k);
+  for (int i = 0; i < n; ++i) scanf("%d", &q[i]);
+  cout << quick_sort(0, n - 1, k) << endl;
+  return 0;
 }
--- 01acwing/acwing_basic/006/908_maximum_disjoint_interval.cpp
+++ 01acwing/acwing_basic/006/908_maximum_disjoint_interval.cpp
@@ -3,33 +3,31 @@ using namespace std;
 
 #define x first
 #define y second
-const int N = 1e5+10;
-using PII = pair<int,int>;
+const int N = 1e5 + 10;
+using PII = pair<int, int>;
 PII q[N];
 int n;
 
 /*本题与905区间选择代码一摸一样，解释如下：
-    区间选点中是将区间划分为不同的集合，每个集合中各个区间都至少有一个点相交，若选中两个不相交的区间，那么两个区间必定在不同的集合中，
+    区间选点中是将区间划分为不同的集合，每个集合中各个区间都至少有一个点相交，
+    若选中两个不相交的区间，那么两个区间必定在不同的集合中，
     而最大不相交区间数便是集合总数，也就是区间选点的数量，所以两题代码相同
 */
-int main()
-{
-    cin>>n;
-    for(int i=0;i<n;++i)cin>>q[i].x>>q[i].y;
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; ++i) cin >> q[i].x >> q[i].y;
 
-    // 按右端点由小到大进行排序
-    sort(q,q+n,[](const auto& a,const auto& b){
-        return a.y<b.y;
-    });
+  // 按右端点由小到大进行排序
+  sort(q, q + n, [](const auto& a, const auto& b) { return a.y < b.y; });
 
-    int res=1,last=q[0].y;
-    for(int i=1;i<n;++i){
-        // 上一个区间的右端点没有被包含在当前区间内，则更新last为当前区间的右端点，区间数+1
-        if(last<q[i].x){
-            res++;
-            last=q[i].y;
-        }
+  int res = 1, last = q[0].y;
+  for (int i = 1; i < n; ++i) {
+    // 上一个区间的右端点没有被包含在当前区间内，则更新last为当前区间的右端点，区间数+1
+    if (last < q[i].x) {
+      res++;
+      last = q[i].y;
     }
-    cout<<res<<endl;
-    return 0;
+  }
+  cout << res << endl;
+  return 0;
 }
