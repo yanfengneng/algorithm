27f0dd8b0a9e448a32d5a73f908ae7e78a2c5b40
yanfengneng <2412553598@qq.com>
2025-04-10 19:58:21 +0800

    Update Code.

--- 01acwing/acwing_basic/001/785_quick_sort.cpp
+++ 01acwing/acwing_basic/001/785_quick_sort.cpp
@@ -6,40 +6,42 @@ const int N = 1e6 + 10;
 int n;
 int q[N];
 
-void quick_sort(int q[],int l,int r)
-{
-    // 区间内只有一个数或者没有数时，不用划分了
-    if(l>=r)return;
+void quick_sort(int q[], int l, int r) {
+  // 区间内只有一个数或者没有数时，不用划分了
+  if (l >= r) return;
 
-    // c++中+的优先级大于>>。因此先进行相加在进行右移一位
-    // x作为分界点，x有四种取法，l，r，mid，随机
-    // 由于指针i、j交换完一次后都要往中间移动一个位置
-    int i=l-1,j=r+1,x=q[l+r>>1];
-    while(i<j)
-    {
-        // do while 语句先执行do里面的语句，再判断while条件，因此前面要将i和j都移动一个偏移量
-        // i找到大于等于x的，j找到小于等于x的，然后二者swap
-        do ++i;while(q[i]<x);
-        do --j;while(q[j]>x);
-        /*
-        不想写 do while 可以写下面两句
-        while(q[++i]<x);
-        while(q[--j]>x);
-        */
-        if(i<j)swap(q[i],q[j]);
-    }
-    // 递归地处理左右两段
-    quick_sort(q,l,j),quick_sort(q,j+1,r);
+  // c++中+的优先级大于>>。因此先进行相加在进行右移一位
+  // x作为分界点，x有四种取法，l，r，mid，随机
+  // 由于指针i、j交换完一次后都要往中间移动一个位置
+  int i = l - 1, j = r + 1, x = q[l + r >> 1];
+  while (i < j) {
+    // do while
+    // 语句先执行do里面的语句，再判断while条件，因此前面要将i和j都移动一个偏移量
+    // i找到大于等于x的，j找到小于等于x的，然后二者swap
+    do {
+      ++i;
+    } while (q[i] < x);
+    do {
+      --j;
+    } while (q[j] > x);
+    /*
+    不想写 do while 可以写下面两句
+    while(q[++i]<x);
+    while(q[--j]>x);
+    */
+    if (i < j) swap(q[i], q[j]);
+  }
+  // 递归地处理左右两段
+  quick_sort(q, l, j), quick_sort(q, j + 1, r);
 }
 
-int main()
-{
-    scanf("%d",&n);
-    for(int i=0;i<n;++i)scanf("%d",&q[i]);
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) scanf("%d", &q[i]);
 
-    quick_sort(q,0,n-1);
+  quick_sort(q, 0, n - 1);
 
-    for(int i=0;i<n;++i)printf("%d ",q[i]);
+  for (int i = 0; i < n; ++i) printf("%d ", q[i]);
 
-    return 0;
+  return 0;
 }
--- 03DataStructureTemplate/Union_Find.cpp
+++ 03DataStructureTemplate/Union_Find.cpp
@@ -3,71 +3,61 @@
 
 using namespace std;
 
-class UF
-{
-private:
-    //count记录联通分量的数量。parent用来表示父亲节点，即树的双亲表示法。rank用来表示树的高度。
-    int count;
-    vector<int> parent,rank;
-public:
-    UF(int n)
-    {
-        this->count=n;
-        parent.resize(n);
-        rank.resize(n);
-        for(int i=0;i<n;++i)
-        {
-            parent[i]=i;
-            rank[i]=0;
-        }
+class UF {
+ private:
+  // count记录联通分量的数量。parent用来表示父亲节点，即树的双亲表示法。rank用来表示树的高度。
+  int count;
+  vector<int> parent, rank;
+
+ public:
+  UF(int n) {
+    this->count = n;
+    parent.resize(n);
+    rank.resize(n);
+    for (int i = 0; i < n; ++i) {
+      parent[i] = i;
+      rank[i] = 0;
     }
+  }
 
-    // 查询树的根
-    int find(int x)
-    {
-        if(parent[x]==x)return x;
-        // 递归查询x的根，顺便进行路径压缩
-        else parent[x]=find(parent[x]);
+  // 查询树的根
+  int find(int x) {
+    if (parent[x] == x) return x;
+    // 递归查询x的根，顺便进行路径压缩
+    else
+      parent[x] = find(parent[x]);
+  }
+
+  // 合并 x 和 y 所属的集合
+  void unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    // x y 根相同，不需要合并
+    if (x == y) return;
+
+    // rank小的向rank大的连边
+    if (rank[x] < rank[y])
+      parent[x] = y;  // x的父节点连向y
+    else {
+      parent[y] = x;
+      // x y 高度相同，则在连边之后树的高度+1
+      if (rank[x] == rank[y]) rank[x]++;
     }
+    // 连通分量-1
+    count--;
+  }
 
-    // 合并 x 和 y 所属的集合
-    void unite(int x,int y)
-    {
-        x=find(x);
-        y=find(y);
-        // x y 根相同，不需要合并
-        if(x==y)return;
+  // 判断 x 和 y 是否相互连通
+  bool connected(int x, int y) { return find(x) == find(y); }
 
-        // rank小的向rank大的连边
-        if(rank[x]<rank[y])parent[x]=y;// x的父节点连向y
-        else{
-            parent[y]=x;
-            // x y 高度相同，则在连边之后树的高度+1
-            if(rank[x]==rank[y])rank[x]++;
-        }
-        // 连通分量-1
-        count--;
-    }
-
-
-    // 判断 x 和 y 是否相互连通
-    bool connected(int x,int y)
-    {
-        return find(x)==find(y);
-    }
-
-    // 输出联通分量数
-    int size()
-    {
-        return this->count;
-    }
+  // 输出联通分量数
+  int size() { return this->count; }
 };
 
-int main()
-{
-    UF uf(10);
-    cout<<uf.size()<<endl;
-    uf.unite(1,2);
-    cout<<uf.size()<<endl;
-    return 0;
+int main() {
+  UF uf(10);
+  cout << uf.size() << endl;
+  uf.unite(1, 2);
+  cout << uf.size() << endl;
+  return 0;
 }
